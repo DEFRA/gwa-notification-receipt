21f9202459b9b58e8a040cf504c8628e2b3acce6
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-04-08 15:45:48 +0100

    feat: handle errors and check for bearer token

--- MessageReceiptHttpTrigger/index.js
+++ MessageReceiptHttpTrigger/index.js
@@ -1,8 +1,31 @@
 module.exports = async function (context) {
   context.log('Message Receipt HTTP Trigger function activated.')
 
-  const { body } = context.req
-  context.bindings.messagesToSend = body
+  try {
+    const authHeader = context.req.headers.authorization
 
-  context.done()
+    if (authHeader === process.env.NOTIFY_BEARER_HEADER) {
+      const { body } = context.req
+      context.bindings.messagesToSend = body
+      context.res = {
+        status: 200
+      }
+    } else if (!authHeader) {
+      // No token
+      context.res = {
+        body: { error: 'AuthMissing', message: 'Missing authorization header.' },
+        status: 401
+      }
+    } else {
+      // Incorrect token
+      context.res = {
+        body: { error: 'AuthError', message: 'Authorization header is not acceptable.' },
+        status: 403
+      }
+    }
+  } catch (e) {
+    context.log.error(e)
+    // Throwing an error ensures the built-in retry will kick in
+    throw new Error(e)
+  }
 }
