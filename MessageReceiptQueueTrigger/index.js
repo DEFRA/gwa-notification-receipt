035326cca13f9ffbba5328814bcbef0e5d09ccba
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-04-12 11:40:36 +0100

    feat: use ref for receipt id rather than Notify's id

--- MessageReceiptQueueTrigger/index.js
+++ MessageReceiptQueueTrigger/index.js
@@ -1,7 +1,12 @@
 module.exports = async function (context) {
   try {
     const { msgReceipt } = context.bindings
-    context.log('Message Receipt Queue Trigger function activated:\n - QueueItem:', msgReceipt)
+    // context.log('Message Receipt Queue Trigger function activated:\n - QueueItem:', msgReceipt)
+
+    // Reference is our Id, ensure doc is saved as such
+    msgReceipt.notify_id = msgReceipt.id
+    msgReceipt.id = msgReceipt.reference
+    delete msgReceipt.reference
 
     // Save document
     context.bindings.receipt = JSON.stringify(msgReceipt)
