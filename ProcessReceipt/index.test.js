aae8d946b8d805b73975df6b27de23de980b48af
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-04-23 17:44:42 +0100

    feat: add tests for bindings

--- ProcessReceipt/index.test.js
+++ ProcessReceipt/index.test.js
@@ -1,6 +1,11 @@
 const processReceipt = require('./index')
+const functionDef = require('./function')
 
 const context = require('../test/defaultContext')
+const testEnvVars = require('../test/testEnvVars')
+
+const inputBindingName = 'msgReceipt'
+const outputBindingName = 'receipt'
 
 describe('ProcessReceipt function', () => {
   afterEach(() => { jest.clearAllMocks() })
@@ -12,8 +17,8 @@ describe('ProcessReceipt function', () => {
 
     await processReceipt(context)
 
-    expect(context.bindings).toHaveProperty('receipt')
-    const receipt = JSON.parse(context.bindings.receipt)
+    expect(context.bindings).toHaveProperty(outputBindingName)
+    const receipt = JSON.parse(context.bindings[outputBindingName])
     expect(receipt).toMatchObject({ id: reference, notify_id: id })
     expect(receipt).not.toMatchObject({ reference: expect.anything() })
   })
@@ -24,5 +29,28 @@ describe('ProcessReceipt function', () => {
     await expect(processReceipt(context)).rejects.toThrow(Error)
 
     expect(context.log.error).toHaveBeenCalledTimes(1)
+  })
+})
+
+describe('ProcessReceipt bindings', () => {
+  test('input binding is correct', () => {
+    const outputBindings = functionDef.bindings.filter((binding) => binding.direction === 'in')
+    expect(outputBindings).toHaveLength(1)
+
+    const outputBinding = outputBindings[0]
+    expect(outputBinding.name).toEqual(inputBindingName)
+    expect(outputBinding.type).toEqual('queueTrigger')
+    expect(outputBinding.queueName).toEqual(`%${testEnvVars.NOTIFICATION_RECEIPT_QUEUE}%`)
+  })
+
+  test('output binding is correct', () => {
+    const outputBindings = functionDef.bindings.filter((binding) => binding.direction === 'out')
+    expect(outputBindings).toHaveLength(1)
+
+    const outputBinding = outputBindings[0]
+    expect(outputBinding.name).toEqual(outputBindingName)
+    expect(outputBinding.type).toEqual('cosmosDB')
+    expect(outputBinding.databaseName).toEqual('gwa')
+    expect(outputBinding.collectionName).toEqual(`%${testEnvVars.COSMOS_DB_RECEIPTS_CONTAINER}%`)
   })
 })
